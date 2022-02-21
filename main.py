81ee5f6d4e8c52be2dd4e6a511a36524ede76b19
Andrija Grozdanović <andrija.gro@gmail.com>
2022-02-21 20:59:56 +0100

    application

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 
+class Application(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title('TicTacToe')
+        #root.geometry('800x600')
+        #root.resizable(width=False, height=False)
+
 def main():
-    print('hello')
+    app = Application()
+    app.mainloop()
 
 if __name__ == '__main__':
     main()
