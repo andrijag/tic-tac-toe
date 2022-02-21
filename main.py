0fa9c69dbbaeeb2ceea62ee3ebfafe52687c018b
Andrija Grozdanović <andrija.gro@gmail.com>
2022-02-22 00:15:39 +0100

    view

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+class View(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -8,9 +14,14 @@ class Application(tk.Tk):
         self.geometry('200x200')
         self.resizable(width=True, height=True)
 
+        view = View(self)
+        view.pack(fill=tk.BOTH, expand=True)
+
+
 def main():
     app = Application()
     app.mainloop()
+
 
 if __name__ == '__main__':
     main()
