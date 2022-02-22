9939a2ea9bfbc107b89bd7f30760b98241bc70d4
Andrija Grozdanović <andrija.gro@gmail.com>
2022-02-22 13:54:42 +0100

    application.py

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-
-
-class View(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-
-class Application(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title('TicTacToe')
-        self.geometry('200x200')
-        self.resizable(width=True, height=True)
-
-        view = View(self)
-        view.pack(fill=tk.BOTH, expand=True)
+from modules.application import Application
 
 
 def main():
---
+++ modules/application.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+class View(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('TicTacToe')
+        self.geometry('200x200')
+        self.resizable(width=True, height=True)
+
+        view = View(self)
+        view.pack(fill=tk.BOTH, expand=True)
+
