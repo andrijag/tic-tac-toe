079d34cb10ca098675a4cee3435bc2c036db1104
Andrija Grozdanović <andrija.gro@gmail.com>
2022-04-12 05:06:25 +0200

    step down

--- modules/model.py
+++ modules/model.py
@@ -2,6 +2,83 @@ from itertools import cycle
 from abc import ABC, abstractmethod
 
 
+class Subject(ABC):
+    @abstractmethod
+    def attach_observer(self, observer):
+        pass
+
+    @abstractmethod
+    def detach_observer(self, observer):
+        pass
+
+    @abstractmethod
+    def notify_observers(self):
+        pass
+
+
+class Game(Subject):
+    def __init__(self, n_rows, n_columns, connect_n):
+        n_players = 2
+        self.players = [Player(i) for i in range(1, n_players + 1)]
+        self._players_iter = cycle(self.players)
+        self._player = next(self._players_iter)
+        self.board = Board(n_rows, n_columns)
+        self._checker = Checker(self.board, connect_n)
+        self._game_over = False
+        self._observers = []
+
+    def attach_observer(self, observer):
+        self._observers.append(observer)
+
+    def detach_observer(self, observer):
+        self._observers.remove(observer)
+
+    def notify_observers(self):
+        for observer in self._observers:
+            observer.update()
+
+    def tick(self, i, j):
+        if self._legal_move(i, j):
+            self._player.tick(self.board, i, j)
+            if self._winning_move(i, j):
+                self._end_game()
+            else:
+                self._next_turn()
+            self.notify_observers()
+
+    def _legal_move(self, i, j):
+        return not self._game_over and not self.board[i][j]
+
+    def _winning_move(self, i, j):
+        return self._checker.check(i, j)
+
+    def _end_game(self):
+        self._game_over = True
+        self._player.score += 1
+
+    def _next_turn(self):
+        self._player = next(self._players_iter)
+
+    def restart(self):
+        self._players_iter = cycle(self.players)
+        self._player = next(self._players_iter)
+        self.board.reset()
+        self._game_over = False
+        self.notify_observers()
+
+
+class Player:
+    def __init__(self, id_):
+        self.id_ = id_
+        self.score = 0
+
+    def __str__(self):
+        return f"player {self.id_}"
+
+    def tick(self, board, i, j):
+        board[i][j] = self.id_
+
+
 class Board:
     def __init__(self, n_rows, n_columns):
         self._board = [[0 for j in range(n_columns)] for i in range(n_rows)]
@@ -53,80 +130,3 @@ class Checker:
         ):
             return 1 + self._count_consecutive(i, j, di, dj)
         return 0
-
-
-class Player:
-    def __init__(self, id_):
-        self.id_ = id_
-        self.score = 0
-
-    def __str__(self):
-        return f"player {self.id_}"
-
-    def tick(self, board, i, j):
-        board[i][j] = self.id_
-
-
-class Subject(ABC):
-    @abstractmethod
-    def attach_observer(self, observer):
-        pass
-
-    @abstractmethod
-    def detach_observer(self, observer):
-        pass
-
-    @abstractmethod
-    def notify_observers(self):
-        pass
-
-
-class Game(Subject):
-    def __init__(self, n_rows, n_columns, connect_n):
-        n_players = 2
-        self.players = [Player(i) for i in range(1, n_players + 1)]
-        self._players_iter = cycle(self.players)
-        self.player = next(self._players_iter)
-        self.board = Board(n_rows, n_columns)
-        self._checker = Checker(self.board, connect_n)
-        self.game_over = False
-        self.observers = []
-
-    def attach_observer(self, observer):
-        self.observers.append(observer)
-
-    def detach_observer(self, observer):
-        self.observers.remove(observer)
-
-    def notify_observers(self):
-        for observer in self.observers:
-            observer.update()
-
-    def tick(self, i, j):
-        if self._legal_move(i, j):
-            self.player.tick(self.board, i, j)
-            if self._winning_move(i, j):
-                self._end_game()
-            else:
-                self._next_turn()
-            self.notify_observers()
-
-    def _legal_move(self, i, j):
-        return not self.game_over and not self.board[i][j]
-
-    def _winning_move(self, i, j):
-        return self._checker.check(i, j)
-
-    def _end_game(self):
-        self.game_over = True
-        self.player.score += 1
-
-    def _next_turn(self):
-        self.player = next(self._players_iter)
-
-    def restart(self):
-        self._players_iter = cycle(self.players)
-        self.player = next(self._players_iter)
-        self.board.reset()
-        self.game_over = False
-        self.notify_observers()
