d1c1c84d7355509bd079f84e11a3afbb2cacd73e
Andrija Grozdanović <andrija.gro@gmail.com>
2022-04-26 02:19:36 +0200

    mvc observer

--- modules/application.py
+++ modules/application.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-from .view import View
-from .model import Game
 
-N_ROWS = 6
-N_COLUMNS = 7
-CONNECT_N = 3
+from .model import Game
+from .view import View
+from .controller import Controller
+
+N_ROWS = 10
+N_COLUMNS = 10
+CONNECT_N = 5
 
 
 class Application(tk.Tk):
@@ -13,23 +15,17 @@ class Application(tk.Tk):
         self.title("Tic-tac-toe")
         # self.geometry('200x200')
         # self.resizable(width=False, height=False)
-
-        view = View(self, N_ROWS, N_COLUMNS)
-        view.grid(column=0, row=0, sticky="n e s w")
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
         game = Game(N_ROWS, N_COLUMNS, CONNECT_N)
+
+        view = View(self, N_ROWS, N_COLUMNS)
+        view.grid(column=0, row=0, sticky="nesw")
+
+        controller = Controller(game, view)
+
+        view.subject = view.model = game
+        view.controller = controller
+        view.update_()
         game.attach_observer(view)
-        view.subject = game
-        view.update()
-
-        for i in range(N_ROWS):
-            for j in range(N_COLUMNS):
-                view.board.spaces[i][j].bind("<Button-1>", lambda event, x=i, y=j: game.tick(x, y))
-
-        view.restart_button.configure(command=game.restart)
-
-        self.player_shape = {
-            player.id_: view.shapes[i] for i, player in enumerate(game.players)
-        }
