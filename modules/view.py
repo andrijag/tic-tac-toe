1ed40f208a1b985ffda105e0b3601d8f7867a58c
Andrija Grozdanović <andrija.gro@gmail.com>
2022-05-01 00:05:07 +0200

    grid in canvas

--- modules/view.py
+++ modules/view.py
@@ -10,37 +10,37 @@ class Observer(ABC):
 
 
 class View(ttk.Frame, Observer):
-    def __init__(self, root, n_rows, n_columns):
-        super().__init__(root)
-        self.subject = self.model = None
+    def __init__(self, parent, n_rows, n_columns):
+        super().__init__(parent)
         self.controller = None
 
         self.shapes = [Cross(), Circle()]
-        self.colors = ["red", "blue", "green"]
+        self.colors = ["red", "blue"]
 
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.rowconfigure(2, weight=1)
 
-        self.score = ttk.Label(self, text="[score]")
+        # self.score = ttk.Label(self, text="score")
+        self.score = ScoreBoard(self)
         self.score.grid(column=0, row=0, padx=10, pady=10)
 
-        square_width = 50
+        square_width = 70
         canvas_width = square_width * n_columns
         canvas_height = square_width * n_rows
         self.board = tk.Canvas(self, width=canvas_width, height=canvas_height)
         self.board_buttons = [[0 for _ in range(n_columns)] for _ in range(n_rows)]
         for i in range(n_rows):
             for j in range(n_columns):
-                x0 = square_width * j + 1
-                y0 = square_width * i + 1
-                x1 = square_width * (j + 1)
-                y1 = square_width * (i + 1)
-                id_ = self.board.create_rectangle((x0, y0), (x1, y1), fill="white")
+                x0 = j * square_width
+                y0 = i * square_width
+                x1 = x0 + square_width
+                y1 = y0 + square_width
+                id_ = self.board.create_rectangle(x0, y0, x1, y1, width=2, fill="white")
                 self.board.tag_bind(id_, "<Button-1>", lambda event, x=i, y=j: self._click(x, y))
                 self.board_buttons[i][j] = id_
-        self.board.create_rectangle((1, 1), (canvas_width, canvas_height), width=5)
+        self.board.create_rectangle(0, 0, canvas_width, canvas_height, width=5)
         self.board.grid(column=0, row=1, padx=10, pady=10)
 
         self.restart_button = ttk.Button(self, text="Restart", command=self._restart)
@@ -48,7 +48,7 @@ class View(ttk.Frame, Observer):
 
     def _click(self, i, j):
         if self.controller:
-            self.controller.tick(i, j)
+            self.controller.click(i, j)
 
     def _restart(self):
         if self.controller:
@@ -59,57 +59,12 @@ class View(ttk.Frame, Observer):
             self.controller.update()
 
 
-class Score(ttk.Label):
+class ScoreBoard(ttk.Label):
     def __init__(self, parent):
-        super().__init__(parent, text="[score]")
+        super().__init__(parent, text="score")
 
-
-class BoardView(tk.Canvas):
-    def __init__(self, parent, n_rows, n_columns):
-        self.square_width = 50
-        width = self.square_width * n_columns
-        height = self.square_width * n_rows
-        super().__init__(parent, width=width, height=height)  # , borderwidth=2, relief="solid")
-        self._board = [[None for _ in range(n_columns)] for _ in range(n_rows)]
-
-        # self.create_rectangle((1, 1), (self.square_width * n_columns, self.square_width * n_rows), width=5,
-        #                       outline='black')
-        # self.tag_bind(id_, "<Button-1>", lambda event: print('clicked'))
-
-    def __getitem__(self, index):
-        return self._board[index]
-
-    def field(self, i, j):
-        width = 50
-        x0 = width * i + 1
-        y0 = width * j + 1
-        x1 = width * (i + 1)
-        y1 = width * (j + 1)
-        return self.create_rectangle((x0, y0), (x1, y1), fill="white")
-
-
-class SquareField:
-    def __init__(self, canvas, *args, **kwargs):
-        self.canvas = canvas
-        self.args = args
-        self.kwargs = kwargs
-
-    def grid(self, row, column):
-        width = self.canvas.width
-        x0 = width * column + 1
-        y0 = width * row + 1
-        x1 = width * (column + 1)
-        y1 = width * (row + 1)
-        self.canvas.create_rectangle((x0, y0), (x1, y1), fill="white")
-
-
-class Space(tk.Canvas):
-    def __init__(self, parent):
-        super().__init__(parent, width=50, height=50)
-        self.empty_color = self["background"]
-
-    def update_(self):
-        pass
+    def update_(self, score):
+        self.configure(text=score)
 
 
 class Shape(ABC):
@@ -119,7 +74,7 @@ class Shape(ABC):
         pass
 
     @abstractmethod
-    def draw(self, canvas):
+    def draw(self, *args, **kwargs):
         pass
 
 
@@ -133,8 +88,8 @@ class Cross(Shape):
     def color(self):
         return self._color
 
-    def draw(self, canvas):
-        pass
+    def draw(self, canvas, *args, **kwargs):
+        canvas.create_line(*args, **kwargs, width=5, outline=self.color)
 
 
 class Circle(Shape):
@@ -147,5 +102,5 @@ class Circle(Shape):
     def color(self):
         return self._color
 
-    def draw(self, canvas):
-        pass
+    def draw(self, canvas, x0, y0, x1, y1, *args, **kwargs):
+        canvas.create_line(*args, **kwargs, width=5, outline=self.color)
