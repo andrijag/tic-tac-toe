aade66fac2a2d69a20d278f12c13a9273f28b1d5
Andrija Grozdanović <andrija.gro@gmail.com>
2022-05-01 00:05:57 +0200

    controller

--- modules/controller.py
+++ modules/controller.py
@@ -3,11 +3,11 @@ from abc import ABC, abstractmethod
 
 class Strategy(ABC):
     @abstractmethod
-    def restart(self):
+    def click(self, i, j):
         pass
 
     @abstractmethod
-    def tick(self, i, j):
+    def restart(self):
         pass
 
     @abstractmethod
@@ -16,32 +16,36 @@ class Strategy(ABC):
 
 
 class Controller(Strategy):
-    def __init__(self, game, view):
-        self.model = game
+    def __init__(self, model, view):
+        self.model = model
         self.view = view
+
         self.player_shape = {
-            player.id_: self.view.shapes[i] for i, player in enumerate(self.model.players)
+            player.id_: view.shapes[i] for i, player in enumerate(model.players)
         }
         self.color_player = {
             player.id_: self.view.colors[i] for i, player in enumerate(self.model.players)
         }
 
+    def click(self, i, j):
+        if self.model:
+            self.model.tick(i, j)
+
     def restart(self):
         if self.model:
             self.model.restart()
 
-    def tick(self, i, j):
-        if self.model:
-            self.model.tick(i, j)
-
     def update(self):
         score = " : ".join(str(player.score) for player in self.model.players)
-        self.view.score.configure(text=score)
+        # self.view.score.configure(text=score)
+        self.view.score.update_(score)
+
         for i in range(self.model.board.n_rows):
             for j in range(self.model.board.n_columns):
+                button_id = self.view.board_buttons[i][j]
                 if self.model.board[i][j]:
-                    self.view.board.itemconfig(self.view.board_buttons[i][j],
-                                               fill=self.color_player[self.model.board[i][j]])
+                    id_ = self.model.board[i][j]
+                    color = self.color_player[id_]
+                    self.view.board.itemconfig(button_id, fill=color)
                 else:
-                    self.view.board.itemconfig(self.view.board_buttons[i][j],
-                                               fill="white")
+                    self.view.board.itemconfig(button_id, fill="white")
