634a4686192649d7470db5e057269d5c30bab170
Andrija Grozdanović <andrija.gro@gmail.com>
2022-05-06 03:00:35 +0200

    kwargs

--- modules/shapes.py
+++ modules/shapes.py
@@ -12,21 +12,15 @@ class Shape(ABC):
 
 
 class Cross(Shape):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.kwargs = {"width": 10, "fill": self.color}
-
     def draw(self, canvas, x0, y0, x1, y1):
+        kwargs = {"width": 10, "fill": self.color}
         return [
-            canvas.create_line(x0, y0, x1, y1, **self.kwargs),
-            canvas.create_line(x0, y1, x1, y0, **self.kwargs),
+            canvas.create_line(x0, y0, x1, y1, **kwargs),
+            canvas.create_line(x0, y1, x1, y0, **kwargs),
         ]
 
 
 class Circle(Shape):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.kwargs = {"width": 9, "outline": self.color}
-
     def draw(self, canvas, x0, y0, x1, y1):
-        return [canvas.create_oval(x0, y0, x1, y1, **self.kwargs)]
+        kwargs = {"width": 9, "outline": self.color}
+        return [canvas.create_oval(x0, y0, x1, y1, **kwargs)]
