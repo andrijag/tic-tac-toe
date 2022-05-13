81762600dfedf516836f0612d3cd39b1503b7261
Andrija Grozdanović <andrija.gro@gmail.com>
2022-05-13 20:04:24 +0200

    BoardSquare.bind(...)

--- modules/view.py
+++ modules/view.py
@@ -11,28 +11,22 @@ class Observer(ABC):
 
 
 class View(ttk.Frame, Observer):
-    def __init__(self, parent, n_rows, n_columns, square_width):
+    def __init__(self, parent, n_rows, n_columns):
         super().__init__(parent)
         self.controller = None
-
         self.shapes = [Cross("blue", "light blue"), Circle("red", "pink")]
+
+        self.score = ScoreBoard(self)
+        self.board = BoardView(self, n_rows, n_columns, command=self._click)
+        for i in range(n_rows):
+            for j in range(n_columns):
+                self.board[i][j].bind("<Button-1>", lambda event, x=i, y=j: self._click(x, y))
+        self.restart_button = ttk.Button(self, text="Restart", command=self._restart)
 
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.rowconfigure(2, weight=1)
-
-        self.score = ScoreBoard(self)
-
-        self.board = BoardView(self, n_rows, n_columns, square_width)
-        for i in range(n_rows):
-            for j in range(n_columns):
-                board_square = self.board[i][j]
-                command = lambda event, x=i, y=j: self._click(x, y)
-                self.board.tag_bind(board_square.id_, "<Button-1>", command)
-
-        self.restart_button = ttk.Button(self, text="Restart", command=self._restart)
-
         self.score.grid(column=0, row=0, padx=10, pady=10)
         self.board.grid(column=0, row=1, padx=10, pady=10)
         self.restart_button.grid(column=0, row=2, padx=10, pady=10)
@@ -59,20 +53,20 @@ class ScoreBoard(ttk.Label):
 
 
 class BoardView(tk.Canvas):
-    def __init__(self, master, n_rows, n_columns, square_width):
+    def __init__(self, master, n_rows, n_columns, command=None):
+        square_width = 100
         canvas_width = square_width * n_columns
         canvas_height = square_width * n_rows
         super().__init__(master, width=canvas_width, height=canvas_height)
 
         self._board = [[None for _ in range(n_columns)] for _ in range(n_rows)]
-        ipad = square_width // 7
         for i in range(n_rows):
             for j in range(n_columns):
                 x0 = j * square_width
                 y0 = i * square_width
                 x1 = x0 + square_width
                 y1 = y0 + square_width
-                self._board[i][j] = BoardSquare(self, x0, y0, x1, y1, ipad=ipad)
+                self._board[i][j] = BoardSquare(self, x0, y0, x1, y1)
         self.create_rectangle(0, 0, canvas_width, canvas_height, width=5)
 
     def __getitem__(self, index):
@@ -80,30 +74,31 @@ class BoardView(tk.Canvas):
 
 
 class BoardSquare:
-    def __init__(self, canvas, x0, y0, x1, y1, ipad=0):
+    def __init__(self, canvas, x0, y0, x1, y1, ipad=10):
         self.canvas = canvas
+        self.id_ = canvas.create_rectangle(x0, y0, x1, y1, width=2, fill="white")
         self.x0 = x0 + ipad
         self.y0 = y0 + ipad
         self.x1 = x1 - ipad
         self.y1 = y1 - ipad
-        self.id_ = canvas.create_rectangle(x0, y0, x1, y1, width=2, fill="white")
         self.shape = []
-        self.shape_width = (x1 - x0) // 7
+
+    def bind(self, event, command):
+        self.canvas.tag_bind(self.id_, event, command)
 
     def draw_shape(self, shape):
         self._erase_shape()
-        args = (self.canvas, self.x0, self.y0, self.x1, self.y1)
-        ids = shape.draw(*args, width=self.shape_width)
+        ids = shape.draw(self.canvas, self.x0, self.y0, self.x1, self.y1)
         self.shape.extend(ids)
-
-    def erase(self):
-        self._erase_shape()
-        self._fill("white")
 
     def _erase_shape(self):
         for id_ in self.shape:
             self.canvas.delete(id_)
         self.shape.clear()
+
+    def erase(self):
+        self._erase_shape()
+        self._fill("white")
 
     def _fill(self, color):
         self.canvas.itemconfigure(self.id_, fill=color)
