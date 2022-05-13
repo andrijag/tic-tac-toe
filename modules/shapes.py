ccc6e855cda1e0888947fa35104595a4f4cf428a
Andrija Grozdanović <andrija.gro@gmail.com>
2022-05-13 20:03:25 +0200

    refactor

--- modules/shapes.py
+++ modules/shapes.py
@@ -7,20 +7,18 @@ class Shape(ABC):
         self.highlight = highlight
 
     @abstractmethod
-    def draw(self, canvas, x0, y0, x1, y1, width=1):
+    def draw(self, canvas, x0, y0, x1, y1):
         pass
 
 
 class Cross(Shape):
-    def draw(self, canvas, x0, y0, x1, y1, width=1):
-        kwargs = {"width": width, "fill": self.color}
+    def draw(self, canvas, x0, y0, x1, y1):
         return [
-            canvas.create_line(x0, y0, x1, y1, **kwargs),
-            canvas.create_line(x0, y1, x1, y0, **kwargs),
+            canvas.create_line(x0, y0, x1, y1, width=10, fill=self.color),
+            canvas.create_line(x0, y1, x1, y0, width=10, fill=self.color),
         ]
 
 
 class Circle(Shape):
-    def draw(self, canvas, x0, y0, x1, y1, width=1):
-        kwargs = {"width": width, "outline": self.color}
-        return [canvas.create_oval(x0, y0, x1, y1, **kwargs)]
+    def draw(self, canvas, x0, y0, x1, y1):
+        return [canvas.create_oval(x0, y0, x1, y1, width=11, outline=self.color)]
