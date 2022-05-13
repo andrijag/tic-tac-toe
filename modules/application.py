8826561947c4bf8a9465247a18bc2f31b15b6245
Andrija Grozdanović <andrija.gro@gmail.com>
2022-05-13 20:03:55 +0200

    rm square_width

--- modules/application.py
+++ modules/application.py
@@ -7,8 +7,6 @@ N_ROWS = 3
 N_COLUMNS = 3
 CONNECT_N = 3
 
-SQUARE_WIDTH = 75
-
 
 class Application(tk.Tk):
     def __init__(self):
@@ -18,7 +16,7 @@ class Application(tk.Tk):
         self.rowconfigure(0, weight=1)
 
         model = Game(N_ROWS, N_COLUMNS, CONNECT_N)
-        view = View(self, N_ROWS, N_COLUMNS, SQUARE_WIDTH)
+        view = View(self, N_ROWS, N_COLUMNS)
         controller = Controller(model, view)
 
         view.controller = controller
