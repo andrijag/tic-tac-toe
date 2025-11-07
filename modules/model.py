38c5ecbb4d9df00ad4b5c180e3a3b13700ac946e
andrija <andrija.gro@gmail.com>
2025-11-07 20:39:03 +0100

    Rename Subject to Observable

--- modules/model.py
+++ modules/model.py
@@ -3,7 +3,7 @@ from enum import Enum
 from itertools import cycle
 
 
-class Subject:
+class Observable:
     def __init__(self) -> None:
         self._observers = []
 
@@ -24,7 +24,7 @@ class Observer(ABC):
         pass
 
 
-class TicTacToe(Subject):
+class TicTacToe(Observable):
     def __init__(
         self,
         n_rows: int = 3,
